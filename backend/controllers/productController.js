b86bccb941ce678df3822c03c34b4424ec295365
snakeeyes007 <1805809@kiit.ac.in>
2022-03-03 01:40:49 +0530

    Added files

---
+++ backend/controllers/productController.js
@@ -0,0 +1,65 @@
+const Product = require("../models/productModel")
+
+
+exports.createProduct = async (req,res,next)=>{
+    const product = await Product.create(req.body)
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+exports.getAllProducts = async(req,res)=>{
+    const products = await Product.find()
+    res.status(201).json({
+        success:true,
+        products
+    })
+}
+
+exports.updateProduct = async(req,res,next)=>{
+    let product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true,
+    runValidators:true,
+    useFindAndModify:false})
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+exports.deleteProduct = async(req,res,next)=>{
+  const product = await Product.findById(req.params.id)
+  if(!product){
+      return res.status(500).json({
+          succes:false,
+          message:"Product not found"
+      })
+  }
+  await product.remove()
+  res.status(200).json({
+      succes:true,
+      message:"Prodcut deleted succesfully"
+  })
+}
+
+exports.getProductDetails = async(req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(500).json({
+            succes:false,
+            message:"Product not found"
+        })
+    }
+    res.status(200).json({
+        succes:true,
+        product
+    })
+}
